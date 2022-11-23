b7494d1574409192aa48170a9c1412f01737c0b0
Teemu Mäkinen <teemu.makinen@vincit.fi>
2022-11-23 10:44:27 +0200

    Add REST endpoints for Hastus export

--- src/main/kotlin/fi/hsl/jore4/hastus/api/ExportController.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/api/ExportController.kt
@@ -1,12 +1,70 @@
 package fi.hsl.jore4.hastus.api
 
-import org.springframework.web.bind.annotation.GetMapping
+import com.fasterxml.jackson.annotation.JsonFormat
+import fi.hsl.jore4.hastus.graphql.GraphQLService
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
-class ExportController {
-    @GetMapping
-    fun helloWorld(): Map<String, String> {
-        return mapOf<String, String>("content" to "Hello World")
+@RequestMapping("export/")
+class ExportController(
+    private val graphQLService: GraphQLService
+) {
+
+    companion object {
+        const val CSV_TYPE = "text/csv"
+    }
+    data class Lines(
+        val labels: List<String>,
+        val priority: Int,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") val observationDate: LocalDate
+    )
+
+    data class Routes(
+        val labels: List<String>,
+        val priority: Int,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") val observationDate: LocalDate
+    )
+
+    @PostMapping("lines", consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [CSV_TYPE])
+    fun exportForLines(
+        @RequestBody request: Lines,
+        @RequestHeader(HttpHeaders.AUTHORIZATION, required = false) authHeader: String?,
+        @RequestHeader("x-hasura-role", required = false) hasuraRole: String?,
+        @RequestHeader("x-hasura-admin-secret", required = false) hasuraSecret: String?
+    ): String {
+        println(hasuraRole)
+        return graphQLService.getRoutesForLines(
+            request.labels,
+            request.priority,
+            request.observationDate,
+            authHeader,
+            hasuraRole,
+            hasuraSecret
+        )
+    }
+
+    @PostMapping("routes", consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [CSV_TYPE])
+    fun exportForRoutes(
+        @RequestBody request: Routes,
+        @RequestHeader(HttpHeaders.AUTHORIZATION, required = false) authHeader: String?,
+        @RequestHeader("x-hasura-role", required = false) hasuraRole: String?,
+        @RequestHeader("x-hasura-admin-secret", required = false) hasuraSecret: String?
+    ): String {
+        println(hasuraRole)
+        return graphQLService.getRoutesForRoutes(
+            request.labels,
+            request.priority,
+            request.observationDate,
+            authHeader,
+            hasuraRole,
+            hasuraSecret
+        )
     }
 }
--- src/test/kotlin/fi/hsl/jore4/hastus/test/RestTest.kt
+++ src/test/kotlin/fi/hsl/jore4/hastus/test/RestTest.kt
@@ -1,13 +1,17 @@
 package fi.hsl.jore4.hastus.test
 
-import fi.hsl.jore4.hastus.api.ExportController
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
 @DisplayName("Test the rest API")
-class RestTest {
+class RestTest @Autowired constructor() {
 
+    val objectMapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+
+    /*
     val ec: ExportController = ExportController()
 
     @Nested
@@ -17,7 +21,27 @@ class RestTest {
         @Test
         @DisplayName("When a GET request is sent")
         fun whenDoingGetRequest() {
-            assert(ec.helloWorld().size == 1)
+            // assert(ec.helloWorld().size == 1)
+        }
+
+        @Test
+        @DisplayName("When forming JSON")
+        fun testJsonForm() {
+            val jsonString = """
+            {
+                "labels": ["65x", "65y"],
+                "priority": 20,
+                "observationDate": "2022-10-24"
+            }
+            """.trimMargin()
+
+            val routes: ExportController.Routes = objectMapper.readValue(jsonString)
+
+            assert(routes.labels[0] == "65x")
+            assert(routes.labels[1] == "65y")
+            assert(routes.priority == 20)
+            assert(routes.observationDate == LocalDate.of(2022, 10, 24))
         }
     }
+     */
 }
