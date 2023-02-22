3d1ed0611aa10c8f3fa67ba04c8883b9f5de0c9c
Tamas Pallos <tamas.pallos@vincit.fi>
2023-02-22 14:55:07 +0200

    Update the local docker-compose setup

--- development.sh
+++ development.sh
@@ -12,22 +12,12 @@ mkdir -p ./docker
 DOCKER_COMPOSE_CMD="docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml"
 
 function check_docker {
-  # compare versions
-  GITHUB_VERSION=$(curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/RELEASE_VERSION.txt --silent)
-  LOCAL_VERSION=$(cat ./docker/RELEASE_VERSION.txt || echo "unknown")
-
-  # download latest version of the docker-compose package in case it has changed
-  if [ "$GITHUB_VERSION" != "$LOCAL_VERSION" ]; then
-    echo "E2E docker-compose package is not up to date, downloading a new version."
-    curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/e2e-docker-compose.tar.gz --silent | tar -xzf - -C ./docker/
-  else
-    echo "E2E docker-compose package is up to date, no need to download new version."
-  fi
+  curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
 }
 
 function start {
   check_docker
-  $DOCKER_COMPOSE_CMD up --build -d jore4-hastus
+  $DOCKER_COMPOSE_CMD up --build -d jore4-hastus jore4-hasura jore4-testdb
 }
 
 function stop_all {
