35a3841987ac74e4d0b70b3c66552e44711d8582
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-02-23 16:53:37 +0200

    Added distances, changed format
    - Added support for stop distances
    - Lines API is not used and removed as unnecessary complication
    - Changed project structure
    - Updated tests to reflect changes

--- src/test/kotlin/fi/hsl/jore4/hastus/test/RestTest.kt
+++ src/test/kotlin/fi/hsl/jore4/hastus/test/RestTest.kt
@@ -3,27 +3,28 @@ package fi.hsl.jore4.hastus.test
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import fi.hsl.jore4.hastus.api.ExportController
+import fi.hsl.jore4.hastus.graphql.GraphQLService
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDate
 
 @DisplayName("Test the rest API")
 class RestTest @Autowired constructor() {
 
     val objectMapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
-    /*
-    val ec: ExportController = ExportController()
+    private val gqService = Mockito.mock(GraphQLService::class.java)
+
+    val ec: ExportController = ExportController(gqService)
 
     @Nested
-    @DisplayName("GET request")
-    inner class GetRequest() {
-
-        @Test
-        @DisplayName("When a GET request is sent")
-        fun whenDoingGetRequest() {
-            // assert(ec.helloWorld().size == 1)
-        }
-
+    @DisplayName("POST request")
+    inner class PostRequest() {
         @Test
         @DisplayName("When forming JSON")
         fun testJsonForm() {
@@ -43,5 +44,4 @@ class RestTest @Autowired constructor() {
             assert(routes.observationDate == LocalDate.of(2022, 10, 24))
         }
     }
-     */
 }
