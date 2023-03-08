f147aac3f0cc04963e1dfc3f6d464a8ec60ea64e
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-03-08 17:47:09 +0200

    Add importer Jore converter and types

---
+++ src/main/kotlin/fi/hsl/jore4/hastus/data/jore/JoreJourneyPattern.kt
@@ -0,0 +1,17 @@
+package fi.hsl.jore4.hastus.data.jore
+
+import java.util.UUID
+
+/**
+ * Jore journey pattern
+ *
+ * @property uniqueLabel Unique label of the route+variant of the journey pattern
+ * @property journeyPatternId UUID of the journey pattern
+ * @property typeOfLine Type of the line the journey patter is on, for example "regional_bus_service"
+ * @property stops List of stop points in the journey pattern
+ */
+data class JoreJourneyPattern(
+    val uniqueLabel: String?,
+    val journeyPatternId: UUID?,
+    val stops: List<JoreStopPoint>
+)
