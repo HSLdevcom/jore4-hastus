e9247c0a93756c12bfd7285cdbbfc74030feb59e
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-03-16 09:11:34 +0200

    Use uniqueLabels instead of labels in export API

--- src/test/kotlin/fi/hsl/jore4/hastus/test/RestTest.kt
+++ src/test/kotlin/fi/hsl/jore4/hastus/test/RestTest.kt
@@ -5,32 +5,26 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import fi.hsl.jore4.hastus.api.ExportController
-import fi.hsl.jore4.hastus.graphql.GraphQLService
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
-import org.springframework.beans.factory.annotation.Autowired
 import java.time.LocalDate
+import kotlin.test.assertEquals
 
 @DisplayName("Test the rest API")
-class RestTest @Autowired constructor() {
+class RestTest {
 
     val objectMapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
-    private val gqService = Mockito.mock(GraphQLService::class.java)
-
-    val ec: ExportController = ExportController(gqService)
-
     @Nested
-    @DisplayName("POST request")
-    inner class PostRequest() {
+    @DisplayName("Object mapper")
+    inner class ObjectMapperTest() {
         @Test
         @DisplayName("When forming JSON")
         fun testJsonForm() {
             val jsonString = """
             {
-                "labels": ["65x", "65y"],
+                "uniqueLabels": ["65x", "65y"],
                 "priority": 20,
                 "observationDate": "2022-10-24"
             }
@@ -38,10 +32,10 @@ class RestTest @Autowired constructor() {
 
             val routes: ExportController.Routes = objectMapper.readValue(jsonString)
 
-            assert(routes.labels[0] == "65x")
-            assert(routes.labels[1] == "65y")
-            assert(routes.priority == 20)
-            assert(routes.observationDate == LocalDate.of(2022, 10, 24))
+            assertEquals("65x", routes.uniqueLabels[0])
+            assertEquals("65y", routes.uniqueLabels[1])
+            assertEquals(20, routes.priority)
+            assertEquals(LocalDate.of(2022, 10, 24), routes.observationDate)
         }
     }
 }
