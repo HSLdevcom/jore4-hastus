c3bd52a5038653528e3bace5fb82bb61314e2822
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-03-16 09:01:43 +0200

    Updated graphQL schema and added queries

---
+++ src/main/kotlin/fi/hsl/jore4/hastus/graphql/converter/DurationConverter.kt
@@ -0,0 +1,14 @@
+package fi.hsl.jore4.hastus.graphql.converter
+
+import com.expediagroup.graphql.client.converter.ScalarConverter
+import kotlin.time.Duration
+
+class DurationConverter : ScalarConverter<Duration> {
+    override fun toJson(value: Duration): String {
+        return value.toIsoString()
+    }
+
+    override fun toScalar(rawValue: Any): Duration {
+        return Duration.parseIsoString(rawValue.toString())
+    }
+}
