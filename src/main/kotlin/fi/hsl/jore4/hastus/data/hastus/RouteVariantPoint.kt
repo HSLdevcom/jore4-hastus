af8b2035d0930815e3fe0cbac4d42b3fdefa3a9b
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-05-25 10:16:47 +0300

    Fix `specTpDistance ` field in CSV export.

--- src/main/kotlin/fi/hsl/jore4/hastus/data/hastus/RouteVariantPoint.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/data/hastus/RouteVariantPoint.kt
@@ -5,8 +5,9 @@ import fi.hsl.jore4.hastus.data.format.NumberWithAccuracy
 /**
  * A Route variant point Hastus element, represents a Jore4 scheduled stop point in journey pattern
  *
- * @property place Name of the hastus place in Jore4
- * @property specTpDistance Distance to the next stop in meters
+ * @property place Name of the Hastus place in Jore4
+ * @property specTpDistance Accumulated distance from the previous timing point in meters.
+ * Must be null, if [isTimingPoint] is false.
  * @property isTimingPoint Is the stop in use, 0 no, 1 yes
  * @property allowLoadTime Is it allowed to wait for other vehicle for loading, 0 no 1 yes
  * @property regulatedTp Is the stop a timing point in Jore4, 0 no 1 yes
@@ -16,7 +17,7 @@ import fi.hsl.jore4.hastus.data.format.NumberWithAccuracy
  */
 data class RouteVariantPoint(
     private val place: String,
-    private val specTpDistance: NumberWithAccuracy,
+    private val specTpDistance: NumberWithAccuracy?,
     private val isTimingPoint: Boolean,
     private val allowLoadTime: Boolean,
     private val regulatedTp: Boolean,
@@ -24,7 +25,21 @@ data class RouteVariantPoint(
     private val routeIdAndVariantId: String
 ) : HastusData() {
 
+    init {
+        require(isTimingPoint || specTpDistance == null) {
+            "specTpDistance must be null when isTimingPoint=false"
+        }
+    }
+
     override fun getFields(): List<Any> {
-        return listOf(place, specTpDistance, isTimingPoint, allowLoadTime, regulatedTp, stopLabel, routeIdAndVariantId)
+        return listOf(
+            place,
+            specTpDistance ?: "", // empty string instead of decimal number if distance not given
+            isTimingPoint,
+            allowLoadTime,
+            regulatedTp,
+            stopLabel,
+            routeIdAndVariantId
+        )
     }
 }
