c7de0b9cfa5d4ff5fda47fcd4bff1d75a5780833
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-06-05 13:42:31 +0300

    Refactor variable names and add explicit types in `ImportController` for better readability.

--- src/main/kotlin/fi/hsl/jore4/hastus/api/ImportController.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/api/ImportController.kt
@@ -1,7 +1,10 @@
 package fi.hsl.jore4.hastus.api
 
 import fi.hsl.jore4.hastus.data.hastus.BookingRecord
+import fi.hsl.jore4.hastus.data.hastus.IHastusData
 import fi.hsl.jore4.hastus.data.hastus.TripRecord
+import fi.hsl.jore4.hastus.data.jore.JoreJourneyPattern
+import fi.hsl.jore4.hastus.data.jore.JoreVehicleScheduleFrame
 import fi.hsl.jore4.hastus.data.mapper.JoreConverter
 import fi.hsl.jore4.hastus.graphql.GraphQLService
 import fi.hsl.jore4.hastus.util.CsvReader
@@ -11,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
+
+private val LOGGER = KotlinLogging.logger {}
 
 @OptIn(ExperimentalTime::class)
 @RestController
@@ -25,40 +31,50 @@ class ImportController(
         const val CSV_TYPE = "text/csv"
     }
 
-    private val LOGGER = KotlinLogging.logger {}
-
     @PostMapping("", consumes = [CSV_TYPE])
     fun importCsvFile(
         @RequestBody request: String,
         @RequestHeader headers: Map<String, String>
     ): String {
+        val reader = CsvReader(";")
+
         val (result, elapsed) = measureTimedValue {
             LOGGER.debug { "CSV import request" }
-            val reader = CsvReader(";")
-            val parsedValues = reader.parseCsv(request)
+
+            val hastusItems: List<IHastusData> = reader.parseCsv(request)
             val filteredHeaders = HeaderUtils.filterInHasuraHeaders(headers)
 
-            val routesOfPatterns = parsedValues.filterIsInstance<TripRecord>().map { it.tripRoute }
-            val name = parsedValues.filterIsInstance<BookingRecord>().first().name
+            val hastusRoutes: List<String> = hastusItems.filterIsInstance<TripRecord>().map { it.tripRoute }
+            val hastusBookingRecordName: String = hastusItems.filterIsInstance<BookingRecord>().first().name
 
-            val journeyPatterns = graphQLService.getJourneyPatternsForRoutes(routesOfPatterns, filteredHeaders)
-            LOGGER.trace { "Importing got journey patterns $journeyPatterns" }
+            val journeyPatternsIndexedByRouteLabel: Map<String, JoreJourneyPattern> =
+                graphQLService.getJourneyPatternsForRoutes(hastusRoutes, filteredHeaders)
+            LOGGER.trace { "Importing got journey patterns $journeyPatternsIndexedByRouteLabel" }
 
-            val vehicleTypes = graphQLService.getVehicleTypes(filteredHeaders)
-            LOGGER.trace { "Importing got vehicle types $vehicleTypes" }
+            val vehicleTypeIndex: Map<Int, UUID> = graphQLService.getVehicleTypes(filteredHeaders)
+            LOGGER.trace { "Importing got vehicle types $vehicleTypeIndex" }
 
-            val dayTypes = graphQLService.getDayTypes(filteredHeaders)
-            LOGGER.trace { "Importing got day types $dayTypes" }
+            val dayTypeIndex: Map<String, UUID> = graphQLService.getDayTypes(filteredHeaders)
+            LOGGER.trace { "Importing got day types $dayTypeIndex" }
 
-            val vehicleScheduleFrame = JoreConverter.convertHastusDataToJore(name, parsedValues, journeyPatterns, vehicleTypes, dayTypes)
+            val vehicleScheduleFrame: JoreVehicleScheduleFrame = JoreConverter.convertHastusDataToJore(
+                hastusBookingRecordName,
+                hastusItems,
+                journeyPatternsIndexedByRouteLabel,
+                vehicleTypeIndex,
+                dayTypeIndex
+            )
+
             graphQLService.persistVehicleScheduleFrame(
-                journeyPatterns.values,
+                journeyPatternsIndexedByRouteLabel.values,
                 vehicleScheduleFrame,
                 filteredHeaders
             )
             "200"
         }
+
         LOGGER.info { "CSV import request took $elapsed" }
+
         return result
     }
 }
