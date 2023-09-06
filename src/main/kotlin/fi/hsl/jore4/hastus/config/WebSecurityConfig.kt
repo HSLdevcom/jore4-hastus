64e25a2b79da92453f3439ffa9b82aaa0cbd492d
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-09-06 16:46:50 +0300

    Fix use of deprecated WebSecurityConfigurerAdapter.

--- src/main/kotlin/fi/hsl/jore4/hastus/config/WebSecurityConfig.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/config/WebSecurityConfig.kt
@@ -1,34 +1,40 @@
 package fi.hsl.jore4.hastus.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig {
 
-    override fun configure(httpSecurity: HttpSecurity) {
-        httpSecurity
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
-            .and()
-            .csrf().disable()
-            .cors().and()
-            .authorizeRequests()
-            .antMatchers(
-                HttpMethod.GET,
-                "/actuator/health"
-            ).permitAll()
-            .antMatchers(
-                HttpMethod.POST,
-                "/import",
-                "/export/routes"
-            )
-            .permitAll()
-            .anyRequest().denyAll()
+    @Bean
+    @Throws(Exception::class)
+    fun configure(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.NEVER) }
+            .csrf { it.disable() }
+            .cors {}
+            .authorizeHttpRequests {
+                it
+                    // GET
+                    .antMatchers(
+                        HttpMethod.GET,
+                        "/actuator/health"
+                    ).permitAll()
+                    // POST
+                    .antMatchers(
+                        HttpMethod.POST,
+                        "/import",
+                        "/export/routes"
+                    ).permitAll()
+                    // other
+                    .anyRequest().denyAll()
+            }
+            .build()
     }
 }
