50a39be1e9eb121867efc69f91f5cdcb084915bd
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-09-20 12:59:21 +0300

    Refactor route variant processing within export feature.
    Make the `variant` field in Hastus `TripRecord` truly nullable (instead of
    empty string), which hinders the understanding of the business logic.

--- src/main/kotlin/fi/hsl/jore4/hastus/data/jore/JoreRoute.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/data/jore/JoreRoute.kt
@@ -4,7 +4,7 @@ import fi.hsl.jore4.hastus.data.format.JoreRouteDirection
 
 data class JoreRoute(
     val label: String,
-    val variant: String,
+    val variant: String?,
     val uniqueLabel: String,
     val name: String,
     val direction: JoreRouteDirection,
