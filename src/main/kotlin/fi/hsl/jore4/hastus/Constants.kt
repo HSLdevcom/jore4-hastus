428c684db441a0470d75617b9a189447ee19ad3a
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-10-04 21:47:01 +0300

    Fix Swedish locale.

--- src/main/kotlin/fi/hsl/jore4/hastus/Constants.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/Constants.kt
@@ -3,7 +3,7 @@ package fi.hsl.jore4.hastus
 object Constants {
 
     const val LANG_FINNISH = "fi_FI"
-    const val LANG_SWEDISH = "se_SE"
+    const val LANG_SWEDISH = "sv_SE"
 
     const val DEFAULT_HASTUS_DATE_FORMAT = "yyyyMMdd"
     const val DEFAULT_HASTUS_TIME_FORMAT = "HHmmss"
