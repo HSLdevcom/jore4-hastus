7f234ed259b1edfa006ace1e3a1097ee31f0f39c
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-10-19 16:39:14 +0300

    Make jore4-hastus return responses in JSON format instead of plain text when importing timetables.
    In case of errors, return the error message also in JSON format and with the
    appropriate status code. It has been argued that until now a 403 status code is
    returned regardless of the nature of the error.
    
    Resolves HSLdevcom/jore4#1552

--- src/main/kotlin/fi/hsl/jore4/hastus/api/ImportController.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/api/ImportController.kt
@@ -3,11 +3,16 @@ package fi.hsl.jore4.hastus.api
 import fi.hsl.jore4.hastus.Constants.MIME_TYPE_CSV
 import fi.hsl.jore4.hastus.service.importing.ImportService
 import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import java.util.UUID
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
@@ -20,23 +25,56 @@ class ImportController(
     private val importService: ImportService
 ) {
 
+    data class ImportTimetablesSuccessResult(
+        val vehicleScheduleFrameId: UUID?
+    )
+
+    data class ImportTimetablesFailureResult(
+        val reason: String?
+    )
+
     @PostMapping("", consumes = [MIME_TYPE_CSV])
     fun importCsvFile(
         @RequestBody csv: String,
         @RequestHeader headers: Map<String, String>
-    ): String {
-        val (result, elapsed) = measureTimedValue {
-            LOGGER.debug { "CSV import request" }
+    ): ImportTimetablesSuccessResult {
+        val (nullableVehicleScheduleFrameId, elapsed) = measureTimedValue {
+            LOGGER.info("Starting to import timetables from CSV file...")
 
             val hasuraHeaders = HeaderUtils.filterInHasuraHeaders(headers)
 
-            importService.importTimetablesFromCsv(csv, hasuraHeaders)
-
-            "200"
+            importService
+                .importTimetablesFromCsv(csv, hasuraHeaders)
+                .also { nullableUuid ->
+                    if (nullableUuid == null) {
+                        // Should never happen, but record log event if it actually does.
+                        LOGGER.warn("CSV import did not result in vehicle schedule frame")
+                    }
+                }
         }
 
-        LOGGER.info { "CSV import request took $elapsed" }
+        LOGGER.info { "CSV import processing completed in $elapsed" }
 
-        return result
+        return ImportTimetablesSuccessResult(nullableVehicleScheduleFrameId)
+    }
+
+    @ExceptionHandler
+    fun handleExportException(ex: Exception): ResponseEntity<ImportTimetablesFailureResult> {
+        return when (ex) {
+            is ResponseStatusException -> {
+                ResponseEntity
+                    .status(ex.statusCode)
+                    .body(ImportTimetablesFailureResult(ex.reason))
+            }
+
+            else -> {
+                LOGGER.error { "Exception during import request:$ex" }
+                LOGGER.error(ex.stackTraceToString())
+
+                ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ImportTimetablesFailureResult(ex.message))
+            }
+        }
     }
 }
