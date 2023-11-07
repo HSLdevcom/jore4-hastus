410bf0205d7bc81fab76c1c5f764a81d0ef59910
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-11-07 15:40:44 +0200

    Fix conversion of Hastus route variant ID in CSV export.
    The bug concerned `rvariant` rows in exported CSV files.
    
    Resolves HSLdevcom/jore4#1591

---
+++ src/test/kotlin/fi/hsl/jore4/hastus/service/exporting/ConversionsToHastusTest.kt
@@ -0,0 +1,47 @@
+package fi.hsl.jore4.hastus.service.exporting
+
+import fi.hsl.jore4.hastus.data.format.JoreRouteDirection
+import fi.hsl.jore4.hastus.service.exporting.ConversionsToHastus.getHastusRouteVariantId
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Test type conversions from Jore to Hastus")
+class ConversionsToHastusTest {
+
+    @DisplayName("Test method: getHastusRouteVariantId")
+    @Nested
+    inner class TestGetHastusRouteVariantId {
+
+        @Test
+        fun `with basic route`() {
+            assertEquals("1", getHastusRouteVariantId("123", "123", null, JoreRouteDirection.OUTBOUND))
+            assertEquals("2", getHastusRouteVariantId("123", "123", null, JoreRouteDirection.INBOUND))
+        }
+
+        @Test
+        fun `with one-char letter variant`() {
+            assertEquals("B1", getHastusRouteVariantId("123", "123B", null, JoreRouteDirection.OUTBOUND))
+            assertEquals("B2", getHastusRouteVariantId("123", "123B", null, JoreRouteDirection.INBOUND))
+        }
+
+        @Test
+        fun `with two-char letter variant`() {
+            assertEquals("BK1", getHastusRouteVariantId("123", "123BK", null, JoreRouteDirection.OUTBOUND))
+            assertEquals("BK2", getHastusRouteVariantId("123", "123BK", null, JoreRouteDirection.INBOUND))
+        }
+
+        @Test
+        fun `with number variant`() {
+            assertEquals("31", getHastusRouteVariantId("123", "123", "3", JoreRouteDirection.OUTBOUND))
+            assertEquals("32", getHastusRouteVariantId("123", "123", "3", JoreRouteDirection.INBOUND))
+        }
+
+        @Test
+        fun `with letter-number variant`() {
+            assertEquals("K41", getHastusRouteVariantId("123", "123K", "4", JoreRouteDirection.OUTBOUND))
+            assertEquals("K42", getHastusRouteVariantId("123", "123K", "4", JoreRouteDirection.INBOUND))
+        }
+    }
+}
