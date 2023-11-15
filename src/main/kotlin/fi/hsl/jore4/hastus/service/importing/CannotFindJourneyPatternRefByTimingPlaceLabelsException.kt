7281810be5dea7231871bba8f6ef063460a43eb3
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-11-15 10:40:54 +0200

    Add integration tests to cover a successful case and some common known exceptions.

--- src/main/kotlin/fi/hsl/jore4/hastus/service/importing/CannotFindJourneyPatternRefByTimingPlaceLabelsException.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/service/importing/CannotFindJourneyPatternRefByTimingPlaceLabelsException.kt
@@ -5,31 +5,34 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 
 class CannotFindJourneyPatternRefByTimingPlaceLabelsException(
-    message: String
+    val routeIdentifier: RouteLabelAndDirection,
+    val stopLabels: List<String>,
+    val placeCodes: List<String?>
 ) : ResponseStatusException(
     HttpStatus.BAD_REQUEST,
-    message
+    constructErrorMessage(routeIdentifier, stopLabels, placeCodes)
 ) {
-
-    constructor(
-        routeIdentifier: RouteLabelAndDirection,
-        stopLabels: List<String>,
-        placeCodes: List<String?>
-    ) : this(
-        """
-        Could not find matching journey pattern reference whose timing place labels correspond to the Hastus trip.
-
-        Trip label: ${routeIdentifier.routeLabel},
-        Trip direction: ${routeIdentifier.direction.wellKnownNumber},
-        Stop points with place codes: ${
-            stopLabels
-                .zip(placeCodes)
-                .map { (stopLabel, nullablePlaceCode) ->
-                    nullablePlaceCode
-                        ?.let { "$stopLabel:$it" }
-                        ?: stopLabel
-                }
+    companion object {
+        private fun constructErrorMessage(
+            routeIdentifier: RouteLabelAndDirection,
+            stopLabels: List<String>,
+            placeCodes: List<String?>
+        ): String {
+            return """
+            Could not find matching journey pattern reference whose timing place labels correspond to the Hastus trip.
+    
+            Trip label: ${routeIdentifier.routeLabel},
+            Trip direction: ${routeIdentifier.direction.wellKnownNumber},
+            Stop points with place codes: ${
+                stopLabels
+                    .zip(placeCodes)
+                    .map { (stopLabel, nullablePlaceCode) ->
+                        nullablePlaceCode
+                            ?.let { "$stopLabel:$it" }
+                            ?: stopLabel
+                    }
+            }
+            """.trimIndent()
         }
-        """.trimIndent()
-    )
+    }
 }
---
+++ src/test/kotlin/fi/hsl/jore4/hastus/service/importing/ImportServiceTest.kt
@@ -0,0 +1,144 @@
+package fi.hsl.jore4.hastus.service.importing
+
+import fi.hsl.jore4.hastus.TestConstants.HASURA_ROLE
+import fi.hsl.jore4.hastus.data.format.JoreRouteDirection
+import fi.hsl.jore4.hastus.data.format.RouteLabelAndDirection
+import fi.hsl.jore4.hastus.test.IntTest
+import fi.hsl.jore4.hastus.test.TimetablesDataInserterRunner
+import fi.hsl.jore4.hastus.test.TimetablesDataset
+import mu.KotlinLogging
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import org.springframework.util.FileCopyUtils
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.UncheckedIOException
+import java.util.UUID
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+
+private val LOGGER = KotlinLogging.logger {}
+
+@IntTest
+class ImportServiceTest(
+    @Autowired private val service: ImportService,
+    @Autowired private val dataInserterRunner: TimetablesDataInserterRunner,
+    @Value("\${hasura.secret}") private val hasuraSecret: String
+) {
+    private fun populateDatabaseFromDataset(pathToFile: String) {
+        val dataset = TimetablesDataset.createFromResource(pathToFile)
+
+        dataInserterRunner
+            .truncateAndInsertDataset(dataset.toJSONString())
+            .also { json ->
+                // Change log level to e.g. DEBUG see what is being persisted into database.
+                LOGGER.trace { "Persisting dataset:\n$json" }
+            }
+    }
+
+    private fun getHasuraHeaders(): Map<String, String> =
+        mapOf(
+            "X-Hasura-Role" to HASURA_ROLE,
+            "X-Hasura-Admin-Secret" to hasuraSecret
+        )
+
+    @Test
+    fun `when journey-pattern-ref exists for both directions`() {
+        populateDatabaseFromDataset("datasets/journey_pattern_ref_100_starting_2023.json")
+
+        val csvToImport: String = readFileAsString("hastus_booking_records/100_both_directions.exp")
+
+        val vsfId: UUID? = service.importTimetablesFromCsv(csvToImport, getHasuraHeaders())
+
+        assertNotNull(vsfId, "Vehicle schedule frame ID must not be null")
+    }
+
+    @Test
+    fun `when the start date of the booking record is earlier than the route of the related journey pattern`() {
+        populateDatabaseFromDataset("datasets/journey_pattern_ref_100_starting_2024.json")
+
+        val csvToImport: String = readFileAsString("hastus_booking_records/100_both_directions.exp")
+
+        assertFailsWith<CannotFindJourneyPatternRefByRouteLabelAndDirectionException> {
+            service.importTimetablesFromCsv(csvToImport, getHasuraHeaders())
+        }.also { exception ->
+            // Fails for the first journey-pattern/route encountered that happens to be the outbound
+            // direction for the line "100" (as this is the first Hastus trip in the file.
+            assertEquals(
+                exception.reason,
+                "Could not find journey pattern reference for Hastus trips with the following route labels and directions: 100 (outbound)"
+            )
+        }
+    }
+
+    @Test
+    fun `when journey-pattern-ref not found for one direction`() {
+        populateDatabaseFromDataset("datasets/journey_pattern_ref_100_outbound_only.json")
+
+        val csvToImport: String = readFileAsString("hastus_booking_records/100_both_directions.exp")
+
+        assertFailsWith<CannotFindJourneyPatternRefByRouteLabelAndDirectionException> {
+            service.importTimetablesFromCsv(csvToImport, getHasuraHeaders())
+        }.also { exception ->
+            assertEquals(
+                exception.reason,
+                "Could not find journey pattern reference for Hastus trips with the following route labels and directions: 100 (inbound)"
+            )
+        }
+    }
+
+    @Test
+    fun `when the stop point labels do not match between Hastus trip and journey-pattern-ref`() {
+        populateDatabaseFromDataset("datasets/journey_pattern_ref_100_with_one_stop_point_label_difference.json")
+
+        val csvToImport: String = readFileAsString("hastus_booking_records/100_both_directions.exp")
+
+        assertFailsWith<CannotFindJourneyPatternRefByStopPointLabelsException> {
+            service.importTimetablesFromCsv(csvToImport, getHasuraHeaders())
+        }.also { exception ->
+            assertEquals(exception.routeIdentifier, RouteLabelAndDirection("100", JoreRouteDirection.OUTBOUND))
+            assertEquals(exception.stopLabels, listOf("H1001", "H1011", "H1021", "H1031"))
+        }
+    }
+
+    @Test
+    fun `when the timing place labels do not match between Hastus trip and journey-pattern-ref`() {
+        populateDatabaseFromDataset("datasets/journey_pattern_ref_100_with_one_timing_place_label_difference.json")
+
+        val csvToImport: String = readFileAsString("hastus_booking_records/100_both_directions.exp")
+
+        assertFailsWith<CannotFindJourneyPatternRefByTimingPlaceLabelsException> {
+            service.importTimetablesFromCsv(csvToImport, getHasuraHeaders())
+        }.also { exception ->
+            assertEquals(exception.routeIdentifier, RouteLabelAndDirection("100", JoreRouteDirection.OUTBOUND))
+            assertEquals(exception.stopLabels, listOf("H1001", "H1011", "H1021", "H1031"))
+            assertEquals(exception.placeCodes, listOf("TP001", null, null, "TP002"))
+        }
+    }
+
+    companion object {
+        private fun readAsString(resource: Resource): String {
+            try {
+                return InputStreamReader(resource.inputStream, Charsets.ISO_8859_1)
+                    .use { reader -> FileCopyUtils.copyToString(reader) }
+            } catch (e: IOException) {
+                throw UncheckedIOException(e)
+            }
+        }
+
+        private fun readFileAsString(pathToFile: String): String {
+            val resourceLoader: ResourceLoader = DefaultResourceLoader()
+            val resource: Resource = resourceLoader.getResource(pathToFile)
+
+            return readAsString(resource)
+                .also { contents ->
+                    LOGGER.debug { "Reading file from path: $pathToFile\n$contents" }
+                }
+        }
+    }
+}
