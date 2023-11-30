0f9fca669ef8b3b25bac6d64f1b89840bcd10007
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-11-30 16:04:18 +0200

    Do minor improvements to `development.sh` e.g. ordering things more logically.

--- development.sh
+++ development.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
 # allow running from any working directory
 WD=$(dirname "$0")
@@ -12,18 +12,21 @@ mkdir -p ./docker
 DOCKER_COMPOSE_CMD="docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml"
 
 function download_docker_bundle {
+  # based on https://github.com/HSLdevcom/jore4-tools#download-docker-bundlesh
+
   echo "Downloading latest version of E2E docker-compose package..."
   curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
 }
 
 function start_all {
   download_docker_bundle
-  $DOCKER_COMPOSE_CMD up --build -d jore4-hastus jore4-hasura jore4-testdb
+  $DOCKER_COMPOSE_CMD up -d jore4-testdb jore4-hasura
+  $DOCKER_COMPOSE_CMD up --build -d jore4-hastus
 }
 
 function start_deps {
   download_docker_bundle
-  $DOCKER_COMPOSE_CMD up --build -d jore4-hasura jore4-testdb
+  $DOCKER_COMPOSE_CMD up -d jore4-testdb jore4-hasura
 }
 
 function stop_all {
@@ -49,19 +52,19 @@ function usage {
   Usage $0 <command>
 
   build
-    Build the project locally
+    Build the Hastus service locally
 
   start
     Start Hastus service in Docker container
 
   start:deps
-    Start only the Docker containers that are dependencies of Hastus service
+    Start only the Docker containers that are dependencies of the Hastus service
 
   stop
-    Stop all Hastus Docker container
+    Stop all Docker containers
 
   remove
-    Stop and remove Hastus Docker container
+    Stop and remove all Docker containers
 
   test
     Run tests locally
@@ -75,6 +78,10 @@ if [[ -z ${1} ]]; then
   usage
 else
   case $1 in
+  build)
+    build
+    ;;
+
   start)
     start_all
     ;;
@@ -91,16 +98,12 @@ else
     remove_all
     ;;
 
-  help)
-    usage
-    ;;
-
-  build)
-    build
-    ;;
-
   test)
     run_tests
+    ;;
+
+  help)
+    usage
     ;;
 
   *)
