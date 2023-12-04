9b6d4657b44a639727264efbfd6e16c5c07a3cec
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-12-04 10:26:26 +0200

    Add array indexing support to `TimetablesDatasetExtensions.getNested(...)`.

--- src/test/kotlin/fi/hsl/jore4/hastus/test/TimetablesDatasetExtensions.kt
+++ src/test/kotlin/fi/hsl/jore4/hastus/test/TimetablesDatasetExtensions.kt
@@ -1,6 +1,9 @@
 package fi.hsl.jore4.hastus.test
 
+import fi.hsl.jore4.hastus.test.TimetablesDatasetExtensions.PATTERN_ARRAY
 import mu.KotlinLogging
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 private val LOGGER = KotlinLogging.logger {}
 
@@ -8,16 +11,66 @@ fun MutableMap<String, Any?>.getNested(propertyPath: String): MutableMap<String,
     val tokens = propertyPath.split(".")
 
     var childProperties: MutableMap<String, Any?> = this
-    for (propertyName in tokens) {
-        LOGGER.debug { "Getting property: $propertyName" }
+    for (token in tokens) {
+        LOGGER.debug { "Getting property: $token" }
 
-        if (propertyName !in childProperties) {
-            throw IllegalStateException("No descendant property was found by name '$propertyName'")
+        val arrayMatcher: Matcher = PATTERN_ARRAY.matcher(token)
+
+        val (propertyName: String, isArraySyntax: Boolean, arrayIndex: Int?) =
+            if (arrayMatcher.matches()) {
+                val arrayPropName: String = arrayMatcher.group(1)
+
+                val index: Int? =
+                    arrayMatcher
+                        .group(2)
+                        .takeUnless { it.isEmpty() }
+                        ?.toInt()
+                        ?.also {
+                            if (it < 0) {
+                                throw IllegalStateException("Index must not be negative: '$token'")
+                            }
+                        }
+
+                Triple(arrayPropName, true, index)
+            } else {
+                Triple(token, false, null)
+            }
+
+        val childElem: Any =
+            childProperties[propertyName]
+                ?: throw IllegalStateException("No descendant property was found by name '$propertyName'")
+
+        when (childElem) {
+            is List<*> -> {
+                if (arrayIndex != null) {
+                    @Suppress("UNCHECKED_CAST")
+                    childProperties = childElem[arrayIndex] as MutableMap<String, Any?>
+                } else {
+                    childProperties = mutableMapOf()
+
+                    childElem.forEachIndexed { index: Int, elem: Any? ->
+                        childProperties[index.toString()] = elem
+                    }
+                }
+            }
+
+            // not a List
+            else -> {
+                if (isArraySyntax) {
+                    throw IllegalStateException("Expected an array but did not get one: '$token'")
+                }
+
+                @Suppress("UNCHECKED_CAST")
+                childProperties = childElem as MutableMap<String, Any?>
+            }
         }
-
-        @Suppress("UNCHECKED_CAST")
-        childProperties = childProperties[propertyName] as MutableMap<String, Any?>
     }
 
     return childProperties
+}
+
+object TimetablesDatasetExtensions {
+    private const val PATTERN_ARRAY_AS_STRING = "(.+)\\[(-?\\d*)]"
+
+    internal val PATTERN_ARRAY: Pattern = Pattern.compile(PATTERN_ARRAY_AS_STRING)
 }
