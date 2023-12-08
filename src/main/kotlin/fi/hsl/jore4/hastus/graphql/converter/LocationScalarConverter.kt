a71f514017a6a78efe8cb31fc8b1a75d84b02981
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-12-08 12:53:51 +0200

    Refactor `LocationScalarConverter` a bit.

--- src/main/kotlin/fi/hsl/jore4/hastus/graphql/converter/LocationScalarConverter.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/graphql/converter/LocationScalarConverter.kt
@@ -1,29 +1,29 @@
 package fi.hsl.jore4.hastus.graphql.converter
 
 import com.expediagroup.graphql.client.converter.ScalarConverter
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import fi.hsl.jore4.hastus.data.format.Coordinate
 
 class LocationScalarConverter : ScalarConverter<Coordinate> {
-    override fun toJson(value: Coordinate): Any {
-        val formattedValue = jacksonObjectMapper().writeValueAsString(
+    override fun toJson(value: Coordinate): ObjectNode {
+        val formattedValue = OBJECT_MAPPER.writeValueAsString(
             mapOf(
                 "type" to "Point",
-                "crs" to mapOf(
-                    "type" to "name",
-                    "properties" to mapOf(
-                        "name" to "urn:ogc:def:crs:EPSG::4326"
-                    )
-                ),
-                "coordinates" to listOf(
-                    value.x,
-                    value.y,
-                    0.0
-                )
+                "crs" to
+                    mapOf(
+                        "type" to "name",
+                        "properties" to
+                            mapOf(
+                                "name" to "urn:ogc:def:crs:EPSG::4326"
+                            )
+                    ),
+                "coordinates" to listOf(value.x, value.y, 0.0)
             )
         )
-        return jacksonObjectMapper().readTree(formattedValue) as ObjectNode
+
+        return OBJECT_MAPPER.readTree(formattedValue) as ObjectNode
     }
 
     override fun toScalar(rawValue: Any): Coordinate {
@@ -33,12 +33,19 @@ class LocationScalarConverter : ScalarConverter<Coordinate> {
         if (rawValue["coordinates"] !is ArrayList<*>) {
             throw IllegalArgumentException("Location has non-map coordinates: " + rawValue["coordinates"])
         }
+
         val numbers = rawValue["coordinates"] as ArrayList<*>
         val x = numbers[0]
         val y = numbers[1]
+
         if (x is Double && y is Double) {
             return Coordinate(x, y)
         }
+
         throw IllegalArgumentException("Location has non-double coordinates: $x and $y")
+    }
+
+    companion object {
+        private val OBJECT_MAPPER: ObjectMapper = jacksonObjectMapper()
     }
 }
