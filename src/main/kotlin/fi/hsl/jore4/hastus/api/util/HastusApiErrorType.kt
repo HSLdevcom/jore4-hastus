6a6838346638f5112f3667ac400d66a3b901ba2b
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-01-25 15:30:41 +0200

    Add type to export API error responses

--- src/main/kotlin/fi/hsl/jore4/hastus/api/util/HastusApiErrorType.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/api/util/HastusApiErrorType.kt
@@ -1,6 +1,9 @@
 package fi.hsl.jore4.hastus.api.util
 
 import fi.hsl.jore4.hastus.graphql.converter.GraphQLAuthenticationFailedException
+import fi.hsl.jore4.hastus.service.exporting.validation.FirstStopNotTimingPointException
+import fi.hsl.jore4.hastus.service.exporting.validation.LastStopNotTimingPointException
+import fi.hsl.jore4.hastus.service.exporting.validation.TooFewStopPointsException
 import fi.hsl.jore4.hastus.service.importing.CannotFindJourneyPatternRefByRouteLabelAndDirectionException
 import fi.hsl.jore4.hastus.service.importing.CannotFindJourneyPatternRefByStopPointLabelsException
 import fi.hsl.jore4.hastus.service.importing.CannotFindJourneyPatternRefByTimingPlaceLabelsException
@@ -12,9 +15,12 @@ enum class HastusApiErrorType {
     CannotFindJourneyPatternRefByStopPointLabelsError,
     CannotFindJourneyPatternRefByTimingPlaceLabelsError,
     ErrorWhileProcessingHastusDataError,
+    FirstStopNotTimingPointError,
     GraphQLAuthenticationFailedError,
     IllegalArgumentError,
     InvalidHastusDataError,
+    LastStopNotTimingPointError,
+    TooFewStopPointsError,
     UnknownError;
 
     companion object {
@@ -23,9 +29,12 @@ enum class HastusApiErrorType {
             is CannotFindJourneyPatternRefByStopPointLabelsException -> CannotFindJourneyPatternRefByStopPointLabelsError
             is CannotFindJourneyPatternRefByTimingPlaceLabelsException -> CannotFindJourneyPatternRefByTimingPlaceLabelsError
             is ErrorWhileProcessingHastusDataException -> ErrorWhileProcessingHastusDataError
+            is FirstStopNotTimingPointException -> FirstStopNotTimingPointError
             is GraphQLAuthenticationFailedException -> GraphQLAuthenticationFailedError
             is IllegalArgumentException -> IllegalArgumentError
             is InvalidHastusDataException -> InvalidHastusDataError
+            is LastStopNotTimingPointException -> LastStopNotTimingPointError
+            is TooFewStopPointsException -> TooFewStopPointsError
             else -> UnknownError
         }
     }
