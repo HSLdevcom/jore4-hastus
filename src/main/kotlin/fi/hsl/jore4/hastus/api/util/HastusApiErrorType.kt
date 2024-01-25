5bf659fc74bc9e19d5371592e786004bbf9d8b3a
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-01-25 15:30:41 +0200

    Add type to export API error responses

--- src/main/kotlin/fi/hsl/jore4/hastus/api/util/HastusApiErrorType.kt
+++ src/main/kotlin/fi/hsl/jore4/hastus/api/util/HastusApiErrorType.kt
@@ -1,6 +1,9 @@
 package fi.hsl.jore4.hastus.api.util
 
 import fi.hsl.jore4.hastus.graphql.converter.GraphQLAuthenticationFailedException
+import fi.hsl.jore4.hastus.service.exporting.validation.FirstStopNotTimingPointException
+import fi.hsl.jore4.hastus.service.exporting.validation.LastStopNotTimingPointException
+import fi.hsl.jore4.hastus.service.exporting.validation.TooFewStopPointsException
 import fi.hsl.jore4.hastus.service.importing.CannotFindJourneyPatternRefByRouteLabelAndDirectionException
 import fi.hsl.jore4.hastus.service.importing.CannotFindJourneyPatternRefByStopPointLabelsException
 import fi.hsl.jore4.hastus.service.importing.CannotFindJourneyPatternRefByTimingPlaceLabelsException
@@ -12,9 +15,12 @@ enum class HastusApiErrorType {
     CannotFindJourneyPatternRefByStopPointLabelsError,
     CannotFindJourneyPatternRefByTimingPlaceLabelsError,
     ErrorWhileProcessingHastusDataError,
+    FirstStopNotTimingPointError,
+    LastStopNotTimingPointError,
     GraphQLAuthenticationFailedError,
     IllegalArgumentError,
     InvalidHastusDataError,
+    TooFewStopPointsError,
     UnknownError;
 
     companion object {
@@ -24,8 +30,11 @@ enum class HastusApiErrorType {
             is CannotFindJourneyPatternRefByTimingPlaceLabelsException -> CannotFindJourneyPatternRefByTimingPlaceLabelsError
             is CannotFindJourneyPatternRefByRouteLabelAndDirectionException -> CannotFindJourneyPatternRefByRouteLabelAndDirectionError
             is ErrorWhileProcessingHastusDataException -> ErrorWhileProcessingHastusDataError
+            is FirstStopNotTimingPointException -> FirstStopNotTimingPointError
+            is LastStopNotTimingPointException -> LastStopNotTimingPointError
             is GraphQLAuthenticationFailedException -> GraphQLAuthenticationFailedError
             is IllegalArgumentException -> IllegalArgumentError
+            is TooFewStopPointsException -> TooFewStopPointsError
             else -> UnknownError
         }
     }
